c6accdcd99a0882dba9dc7022746426a2c6dc67d
Patrycja Witkowska <narginargila@gmail.com>
2018-04-09 15:37:02 +0200

    Changes to script.js

--- js/script.js
+++ js/script.js
@@ -6,15 +6,10 @@ var triangle3Area = getTriangleArea(2, 8);
 
 
 function getTriangleArea (a, h) {
-
-if ((a > 0) || (h > 0)) {  //jeżeli a jest większe od 0 lub h jest większe od zera
-
-return a*h/2 ;
-}
-
-else {return "Nieprawidłowe dane"}  
-
-
+	if ((a > 0) && (h > 0)) {  
+		return a*h/2 ;
+	}
+	else {return "Nieprawidłowe dane"}  
 }
 
 console.log(getTriangleArea(10, 6));
